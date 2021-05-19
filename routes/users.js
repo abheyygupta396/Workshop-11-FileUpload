f6346cb732db26473bc995fe95790fbd8d68ac8d
Abhey gupta <abheyygupta396@gmail.com>
2021-05-19 20:49:28 +0530

    user

---
+++ routes/users.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var router = express.Router();
+const app = express();
+const user = require('./schema');
+const jwt = require('jsonwebtoken');
+
+
+
+// Sample to check api working.
+router.get('/', function (req, res, next) {
+  res.status(200).json({
+    message: ` Hello ${req.body.name} from server `
+  });
+});
+
+
+// 1. Create-user: When the api is called, a user will be created. Take Username and Password. 
+
+router.post('/signup', function (req, res, next) {
+
+  user.findOne({ username: req.body.username })
+    .exec((error, result) => {
+      if (result) {
+        res.status(400).json({
+          message: 'User already registered'
+        });
+      } else {
+        const {
+          username,
+          password
+        } = req.body;
+
+        const _user = new user({
+          username,
+          password,
+
+        });
+
+        _user.save((error, data) => {
+          if (error) {
+            res.status(400).json({
+              message: 'Something went wrong'
+            });
+
+          }
+
+          if (data) {
+            res.status(201).json({
+              result: data
+            });
+
+          }
+
+        });
+      }
+    });
+
+});
+
+//2. login-user: Username and password will be sent to the API. API will return a JWT token.
+
+router.post('/signin', function (req, res, next) {
+
+  user.findOne({ username: req.body.username, password: req.body.password })
+    .exec((error, result) => {
+
+      if (error) {
+        res.status(400).json({ error });
+      }
+      else {
+        // console.log(result);`
+        if (result != null) {
+          const token = jwt.sign({
+            _username: user._username,
+            _password: user._password
+          }, 'secret',
+            { expiresIn: '1h' })
+          res.status(201).json({ result, token });
+
+        }
+        else {
+          res.status(400).json('User Not found');
+        }
+      }
+    });
+});
+
+//3. validate-user: Send token in the header > Authorization Header as a Bearer token. API will return true or false if the token is valid.
+
+router.post('/profile', function (req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  const user = jwt.verify(token, 'secret');
+  if (req.user = user) {
+      // console.log(token);
+      res.status(200).json({ user: 'logged in Succesfully' })
+      //jwt.decode()
+  }
+});
+
+module.exports = router;
