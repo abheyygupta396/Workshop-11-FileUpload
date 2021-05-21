43f53604a1186486163393caf882aa06b69e4c94
Abhey gupta <abheyygupta396@gmail.com>
2021-05-21 20:08:39 +0530

    pagination

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,14 @@ const app = express();
 const user = require('./schema');
 const jwt = require('jsonwebtoken');
 var fileUpload = require('express-fileupload');
+const cron = require('node-cron');
 var upload = require('./multer');
+
 
 // var ObjectId = require('mongodb').ObjectId;
 
 app.use(fileUpload());
+
 
 // Sample to check api working.
 router.get('/', function (req, res, next) {
@@ -89,7 +92,8 @@ router.post('/signin', function (req, res, next) {
     });
 });
 
-//3. validate-user: Send token in the header > Authorization Header as a Bearer token. API will return true or false if the token is valid.
+//3. validate-user: Send token in the header > Authorization Header as a Bearer token. 
+//   API will return true or false if the token is valid, Also for Middleware user
 
 router.post('/profile', function (req, res, next) {
   const token = req.headers.authorization.split(" ")[1];
@@ -102,56 +106,62 @@ router.post('/profile', function (req, res, next) {
 });
 
 
+// Workshop: 12 :
+// 1) MongoDB: Pagination & Sorting:
 
-//1) delete-file: Request Type: DELETE. It should be able to delete a given file ID. 
+router.get('/paginate', async (req, res, next) => {
+  try {
+    let { page, size } = req.query
 
-router.delete('/:userId', function (req, res, next) {
-
-  user.remove({ _id: req.params.userId })
-    .exec()
-    .then(result => {
-      res.status(200).json({ user: 'deleted Successfully' })
-    })
-
-    .catch(err => {
-      console.log("error");
-      res.status(500).json({ error: err });
-    });
-
-});
-
-
-// 2) upload: Request Type: POST.  It should only be able to take a file type JPEG/PNG with size limit of 1mb. The api will return a file object with ID which is saved on the server.
-
-router.post('/upload', upload.single('image'), function (req, res, next) {
-  res.send({
-    success: true,
-    message: 'file uploaded successfully',
-    file: req.file
-  });
-  
-});
-
-// 3) Rename-file: Request Type: PUT. Ot should take a file ID and newName of the file. The code should rename the file. 
-
-router.put('/:userId', function (req, res, next) {
-
-  user.updateOne(
-    { _id: req.params.userId },
-
-    {
-      $set: { username, password } = req.body,
+    if (!page) {
+      page = 1
     }
-  )
-    .then((result) => {
-      res.status(200).json(result)
-      console.log(result);
-    }).catch((err) => { console.log(err) })
+
+    if (!size) {
+      size = 2  //here we can define the size for records displayed on the page:  
+    }
+
+    const limit = parseInt(size)
+    const skip = (page - 1) * size
+
+    const users = await user.find().limit(limit).skip(skip);
+
+    res.send({
+      page,
+      size,
+      data: users,
+    });
+  }
+
+  catch (error) {
+    res.status(500).send(error.message);
+
+  }
+
+
+});
+
+// 2) Mongodb: Search & Selection api :
+
+router.get('/:name', (req, res, next) => {
+
+  var regex = new RegExp(req.params.name, 'i');
+
+  // user.find({"_id": ObjectId(req.params.name)}).then((result) => {
+
+  user.find({ username: regex }).then((result) => {
+
+    res.status(200).json(result)
+    console.log(result);
+  })
+    .catch(err => { console.log(err) })
 
 });
 
 
 
 module.exports = router;
+
+
 
 
